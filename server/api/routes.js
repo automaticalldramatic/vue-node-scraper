3f239d85974c007c80568db0bb83485ef0451fd3
Rizwan Iqbal <mailme@rizwaniqbal.com>
2017-09-24 21:40:55 +0530

    Just some housekeeping, am tired, need a break and will start with Vue setup
    Signed-off-by: Rizwan Iqbal <mailme@rizwaniqbal.com>

--- server/api/routes.js
+++ server/api/routes.js
@@ -3,14 +3,30 @@ const scrape    = require('./controllers/scrape'),
 
 module.exports = function(app) {
 
+    /**
+     * Post route for the API
+     */
     app.post('/scrape', scrape.search)
 
+    /**
+     * Catch all route for all get calls to this server
+     */
     app.get('*', function(req, res, next) {
         var err = new Error();
         err.status = 404;
         next(err);
     });
 
+    /**
+     * Catch all function for other routes
+     * We can improve this with a folder with all error templates
+     *
+     * @param  {object} err
+     * @param  {object} req
+     * @param  {object} res
+     * @param  {object} next
+     * @return {object}
+     */
     app.use(function(err, req, res, next) {
         if(err.status !== 404) {
             return next();
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express   = require('express'),
     app         = express(),
     http        = require('http'),
     routes      = require('./api/routes.js'),
+    // Heroku or AWS EC2 would have this as a part of their environment config
     port        = process.env.PORT || 8081,
     bodyParser  = require('body-parser');
 // const database  = require('./config/database');
@@ -13,6 +14,7 @@ app.use(bodyParser.json());
 // we pass the app instance to this one.
 routes(app);
 
+// Create the server and display a message to indicate we are on
 http.createServer(app).listen(port, function() {
-    console.log('The magic happens at dragonstone.local:' + port);
+    console.log('Demo API server for scraping a web page is up on :' + port);
 });
